61905ec0123bcacca698e5021a05bbf8ca667736
Niruksorp <prge200000@mail.ru>
2021-05-22 12:57:53 +0700

    final

--- app/src/main/java/com/sfu/android_labs/MainActivity.java
+++ app/src/main/java/com/sfu/android_labs/MainActivity.java
@@ -1,18 +1,26 @@
     package com.sfu.android_labs;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+    import android.annotation.SuppressLint;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.os.Handler;
+    import android.os.Looper;
+    import android.view.View;
+    import android.widget.Toast;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.recyclerview.widget.LinearLayoutManager;
+    import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+    import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
-import java.util.List;
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.net.HttpURLConnection;
+    import java.net.URL;
+    import java.util.ArrayList;
+    import java.util.List;
 
     public class MainActivity extends AppCompatActivity {
 
@@ -20,14 +28,40 @@ import java.util.List;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            new Thread(new Runnable() {
+                @SuppressLint("ShowToast")
+                @Override
+                public void run() {
+                    URL url;
+                    HttpURLConnection connection = null;
+
+                    try {
+                        url = new URL("http://10.0.2.2:8080/projects");
+                        connection = (HttpURLConnection) url.openConnection();
+                        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                        String line = br.readLine();
+
+                        new Handler(Looper.getMainLooper()).post(() -> {
+                            Toast.makeText(MainActivity.this, line, Toast.LENGTH_LONG);
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
         RecyclerView recyclerView = findViewById(R.id.activity_main__rv_vacations);
         VacationAdapter vacationAdapter = new VacationAdapter(generateMovieList());
         recyclerView.setAdapter(vacationAdapter);
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(mLayoutManager);
 
+
         FloatingActionButton fab = findViewById(R.id.activity_main__fb_add);
-        Intent intent = new Intent(MainActivity.this, UserProfileActivity.class);
+        Intent intent = new Intent(MainActivity.this, MainCrudActivity.class);
+        // MainCrudActivity UserProfileActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,8 +72,12 @@ import java.util.List;
 
         private List<Vacation> generateMovieList() {
             List<Vacation> list = new ArrayList<>();
-            list.add(new Vacation("some","dede",R.drawable.ic_launcher_background));
-            list.add(new Vacation("some","dede",R.drawable.ic_launcher_background));
+            list.add(new Vacation("С++ Calculator","Пишем самый лучший калькулятор на планете земля, а мб и чуть лучше"
+                    ,R.drawable.ic_launcher_background));
+            list.add(new Vacation("Vue Framework","Напишем свой собственный Вью фреймворк!!",R.drawable.ic_launcher_background));
+            list.add(new Vacation("Vue Framework","Напишем свой собственный Вью фреймворк!!",R.drawable.ic_launcher_background));
+            list.add(new Vacation("Vue Framework","Напишем свой собственный Вью фреймворк!!",R.drawable.ic_launcher_background));
+            list.add(new Vacation("Vue Framework","Напишем свой собственный Вью фреймворк!!",R.drawable.ic_launcher_background));
             return list;
         }
 
